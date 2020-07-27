d4c71001ace1ba8f750b08d40c5fe2594c08285a
K.K <gokennsirou@yahoo.co.jp>
2020-07-28 06:39:56 +0900

    [Add] user show page

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,12 +1,33 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>
-  <!-- <a href="{{ url('/') }}" class="header-menu">Back</a> -->
-  <!-- {{ $post->title }} -->
-  ユーザー詳細
-</h1>
-<p>{{ $user->name }}</p>
-<p>{!! nl2br(e($user->introduction)) !!}</p>
 
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">ユーザー詳細</div>
+
+        <div class="card-body">
+          <div>
+            <h2>ユーザー名</h2>
+            <p>{{ $user->name }}</p>
+          </div>
+          <div>
+            <h2>自己紹介</h2>
+            @if ($user->introduction)
+              <p>{!! nl2br(e($user->introduction)) !!}</p>
+            @else
+              <p>自己紹介文は未登録です。</p>
+            @endif
+          </div>
+        </div>
+
+        <div>
+          <a href="{{ action('UsersController@edit', $user) }}">編集</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
