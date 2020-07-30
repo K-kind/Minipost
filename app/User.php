500e225ef5b75f3b16baed263974b087a9d9cd2e
K.K <gokennsirou@yahoo.co.jp>
2020-07-30 12:13:56 +0900

    [Add] followers table

--- app/User.php
+++ app/User.php
@@ -44,4 +44,14 @@ class User extends Authenticatable
     public function likes() {
         return $this->hasMany('App\Like');
     }
+
+    public function followers()
+    {
+        return $this->belongsToMany(self::class, 'followers', 'followed_id', 'following_id');
+    }
+
+    public function follows()
+    {
+        return $this->belongsToMany(self::class, 'followers', 'following_id', 'followed_id');
+    }
 }
